8fda5b667387f71016b3bcc8fc7409b17e76e99c
Ernesto Ocampo <ernesto@sistrategia.com>
2019-12-16 13:09:54 -0600

    Change Password

--- src/AspNetIdentity221Reference/Controllers/ManageController.cs
+++ src/AspNetIdentity221Reference/Controllers/ManageController.cs
@@ -57,6 +57,32 @@ namespace AspNetIdentity221Reference.Controllers
             return View(model);
         }
 
+        //
+        // GET: /Manage/ChangePassword
+        public ActionResult ChangePassword() {
+            return View();
+        }
+
+        //
+        // POST: /Manage/ChangePassword
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ChangePassword(ChangePasswordViewModel model) {
+            if (!ModelState.IsValid) {
+                return View(model);
+            }
+            var result = await UserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
+            if (result.Succeeded) {
+                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                if (user != null) {
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                }
+                return RedirectToAction("Index", new { Message = ManageMessageId.ChangePasswordSuccess });
+            }
+            AddErrors(result);
+            return View(model);
+        }
+
         protected override void Dispose(bool disposing) {
             if (disposing && _userManager != null) {
                 _userManager.Dispose();
